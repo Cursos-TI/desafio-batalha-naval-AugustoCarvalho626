8cf578ef4a54b5befd765900e53b137080d3a21c
AugustoCarvalho626 <augustojipa2008@hotmail.com>
2025-08-11 02:42:34 +0000

    melhoria concluida

--- Jogo de Batalha Naval/Desafio_aventureiro.c
+++ Jogo de Batalha Naval/Desafio_aventureiro.c
@@ -5,6 +5,11 @@
 // Teste: Augusto Bento Carvalho
 
 
+#define TAMANHO_TABULEIRO 10
+#define AGUA 0
+#define NAVIO 3
+#define TAMANHO_NAVIO 3  
+
 
 int main(){
 
@@ -13,44 +18,44 @@ int main(){
 
 // Preenchendo o tabuleiro com "0". Obs: 0 = Água.
 
-for (int i = 0; i < 10; i++) {      // Loop para as linhas (de 0 a 9).
-    for (int j = 0; j < 10; j++) {      // Loop para as colunas (de 0 a 9).
-        tabela[i][j] = 0; 
-    } 
+for (int i = 0; i < TAMANHO_TABULEIRO; i++) {           // Loop para as linhas (de 0 a 9).
+    for (int j = 0; j < TAMANHO_TABULEIRO; j++) {       // Loop para as colunas (de 0 a 9).
+        tabela[i][j] = AGUA;   
+    }
 } 
 
 
 // Posicionando o primeiro navio na horizontal.
 
-for (int i = 0; i < 3; i++) {       // Loop para posicionar um navio de tamanho 3. 
+for (int i = 0; i < TAMANHO_NAVIO; i++) {       // Loop para posicionar um navio de tamanho 3. 
    
-    tabela[2][3 + i] = 3;           // A linha é sempre 2. A coluna começa em 3 e aumenta a cada passo.  
+    tabela[2][3 + i] = NAVIO;           // A linha é sempre 2. A coluna começa em 3 e aumenta a cada passo.  
 } 
 
      
 // Posicionando o segundo navio na vertical. 
 
-for (int i = 0; i < 3; i++) {       // Loop para posicionar um navio de tamanho 3.
+for (int i = 0; i < TAMANHO_NAVIO; i++) {       // Loop para posicionar um navio de tamanho 3.
     
-    tabela[5 + i][7] = 3;           // A coluna é sempre 7. A linha começa em 5 e aumenta a cada passo. 
+    tabela[5 + i][7] = NAVIO;           // A coluna é sempre 7. A linha começa em 5 e aumenta a cada passo. 
 } 
 
 
 // Posicionando o terceiro navio na diagonal. 
 
-for (int i = 0, j = 0; i < 3 && j < 3; i++, j++) {       // Loop para posicionar um navio de tamanho 3.
+for (int i = 0, j = 0; i < TAMANHO_NAVIO && j < TAMANHO_NAVIO; i++, j++) {       // Loop para posicionar um navio de tamanho 3.
    
 
-    tabela[5 + i][0 + j] = 3;           // A coluna e a linha aumentam a cada passo. 
+    tabela[5 + i][0 + j] = NAVIO;           // A coluna e a linha aumentam a cada passo. 
 } 
 
 
 // Posicionando o quarto navio na diagonal. 
 
-for (int i = 0, j = 0; i < 3 && j < 3; i++, j++) {       // Loop para posicionar um navio de tamanho 3.
+for (int i = 0, j = 0; i < TAMANHO_NAVIO && j < TAMANHO_NAVIO; i++, j++) {       // Loop para posicionar um navio de tamanho 3.
    
 
-    tabela[4 + i][6 - j] = 3;           // A linha aumentam e a coluna diminui a cada passo. 
+    tabela[4 + i][6 - j] = NAVIO;           // A linha aumentam e a coluna diminui a cada passo. 
 } 
 
 
